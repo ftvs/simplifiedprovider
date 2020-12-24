1624756c84f7758113a841eeab639c301c2ff97e
ftvs <akagi.kaede@gmail.com>
2020-12-24 11:41:04 +0800

    Convert unnecessary StatefulWidgets to StatelessWidgets.

--- lib/loaded.dart
+++ lib/loaded.dart
@@ -3,24 +3,18 @@ import 'package:flutter/widgets.dart';
 
 typedef Future<T> Load<T>();
 
-class LoadedView<T> extends StatefulWidget {
+class LoadedView<T> extends StatelessWidget {
   final Load<T> _load;
   final Widget Function(BuildContext context, T model) builder;
 
   LoadedView({Load load, this.builder}) : _load = load;
 
-  @override
-  _LoadedViewState createState() => _LoadedViewState<T>();
-}
-
-class _LoadedViewState<T> extends State<LoadedView<T>> {
-  @override
   Widget build(BuildContext context) {
     return FutureBuilder<T>(
-      future: widget._load(),
+      future: _load(),
       builder: (BuildContext context, AsyncSnapshot<T> snapshot) {
         if (snapshot.hasData) {
-          return widget.builder(context, snapshot.data);
+          return builder(context, snapshot.data);
         } else if (snapshot.hasError) {
           print("load error ${snapshot.error}");
           return Text("Error loading content.");
